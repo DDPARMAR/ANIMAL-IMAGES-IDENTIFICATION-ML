54a2f0f93b5f18a6c8a7c69fbc3f8698be0175c9
DDPARMAR <devp45837@gmail.com>
2023-10-25 15:30:39 +0530

    CODE UPDATED

--- Main.py
+++ Main.py
@@ -9,46 +9,41 @@ relative_path = "DATASET"
 data = os.listdir(relative_path)
 
 labels = []
-feature_vectors = []
+image_data = []
 
 for i in data:
     animal_folder = os.listdir(os.path.join(relative_path, i))
+    
     for j in animal_folder:
         image_path = os.path.join(relative_path, i, j)
 
         print("Reading image:", image_path + " Label: " + i)
 
         image = cv2.imread(image_path)
-        image = cv2.resize(image, (500, 500))  # Resize to a common size
+        image = cv2.resize(image, (600, 600)) 
 
         image = image.flatten()
         labels.append(i)
-        feature_vectors.append(image)
+        image_data.append(image)
 
-# Convert feature_vectors to a numpy array
-X = np.array(feature_vectors)
+
+X = np.array(image_data)
 y = np.array(labels)
 
-# Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-# Create and train the KNN classifier with a specified number of neighbors (k)
-k = 5  # You can change k to your preference
-clf = KNeighborsClassifier(n_neighbors=k)
+clf = KNeighborsClassifier(n_neighbors=5)
 clf.fit(X_train, y_train)
 
-# Make predictions on the test set
 y_pred = clf.predict(X_test)
 
-# Calculate the accuracy
 accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy:", accuracy)
 
-# To make predictions on a new image:
-new_image = cv2.imread("TEST\\testlion1.jpg")  # Use the correct path separator
-new_image = cv2.resize(new_image, (500, 500))  # Resize to match the training data dimensions
+new_image = cv2.imread("TEST\\testlion1.jpg") 
+new_image = cv2.resize(new_image, (600, 600)) 
 
-new_features = new_image.flatten()
+new_image_data = new_image.flatten()
 
-predicted_label = clf.predict([new_features])  # Use the trained classifier
+predicted_label = clf.predict([new_image_data])  
 print("Predicted Label:", predicted_label[0])
